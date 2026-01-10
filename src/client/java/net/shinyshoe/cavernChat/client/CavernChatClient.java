42586e23333f9504729848e3cc800bb7384ea14e
McShinyShoe <mcshinyshoe@protonmail.com>
2026-01-10 19:54:28 +0700

    feat: Connected the configuration with the filter

--- src/client/java/net/shinyshoe/cavernChat/client/CavernChatClient.java
+++ src/client/java/net/shinyshoe/cavernChat/client/CavernChatClient.java
@@ -12,5 +12,29 @@ public class CavernChatClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         AutoConfig.register(CavernChatConfig.class, GsonConfigSerializer::new);
+
+        AutoConfig.getConfigHolder(CavernChatConfig.class).registerSaveListener((holder, config) -> {
+            onConfigChanged(config);
+            return ActionResult.SUCCESS;
+        });
+        AutoConfig.getConfigHolder(CavernChatConfig.class).registerLoadListener((holder, config) -> {
+            onConfigChanged(config);
+            return ActionResult.SUCCESS;
+        });
+    }
+    private static void onConfigChanged(CavernChatConfig config) {
+        ChatFilter.DYNAMIC_FILTERS.clear();
+        for(Map.Entry<String, Supplier<FilterMode>> filter : ChatFilter.CONFIGURABLE_FILTERS.entrySet()) {
+            switch (filter.getValue().get()) {
+                case FilterMode.ALWAYS_OFF -> ChatFilter.FILTERS.get(filter.getKey()).disable();
+                case FilterMode.ALWAYS_ON -> ChatFilter.FILTERS.get(filter.getKey()).enable();
+                case FilterMode.DYNAMIC -> {
+                    ChatFilter.DYNAMIC_FILTERS.add(filter.getKey());
+                    if(ChatFilter.FILTERS.get("other").getStatus()) ChatFilter.FILTERS.get(filter.getKey()).enable();
+                    else ChatFilter.FILTERS.get(filter.getKey()).disable();
+                }
+            }
+        }
+        ChatVisibilityController.chatRebuild();
     }
 }
--- src/client/java/net/shinyshoe/cavernChat/client/ChatFilter.java
+++ src/client/java/net/shinyshoe/cavernChat/client/ChatFilter.java
@@ -4,10 +4,9 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.shinyshoe.cavernChat.client.util.ChatUtils;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class ChatFilter {
     public ChatFilter(String name, Predicate<Text> filter, boolean status) {
@@ -17,6 +16,8 @@ public class ChatFilter {
     }
 
     public static final Map<String, ChatFilter> FILTERS = new LinkedHashMap<>();
+    public static final Map<String, Supplier<FilterMode>> CONFIGURABLE_FILTERS = new LinkedHashMap<>();
+    public static final Set<String> DYNAMIC_FILTERS = new HashSet<>();
 
     static {
         FILTERS.put("global", new ChatFilter("global", (Text text) -> ChatUtils.checkChar(text, '»', Formatting.GRAY.getColorValue()) &&
@@ -29,6 +30,22 @@ public class ChatFilter {
         FILTERS.put("local", new ChatFilter("local", (Text text) -> ChatUtils.checkChar(text, '[', 0xFFD700) && ChatUtils.checkChar(text, '»', Formatting.GRAY.getColorValue()), true));
         FILTERS.put("party", new ChatFilter("party", (Text text) -> ChatUtils.checkChar(text, '[', 0x64F581) && ChatUtils.checkChar(text, '»', Formatting.GRAY.getColorValue()), true));
         FILTERS.put("dm", new ChatFilter("dm", (Text text) -> text.getString().startsWith("✉"), true));
+
+        FILTERS.put("player_join", new ChatFilter("player_join", (Text text) -> text.getString().startsWith("[+]"), true));
+        FILTERS.put("player_leave", new ChatFilter("player_leave", (Text text) -> text.getString().startsWith("[-]"), true));
+        FILTERS.put("new_player", new ChatFilter("new_player", (Text text) -> text.getString().startsWith("Welcome"), true));
+
+        FILTERS.put("vote", new ChatFilter("vote", (Text text) -> text.getString().startsWith("[/VOTE]"), true));
+        FILTERS.put("motd", new ChatFilter("motd", (Text text) -> text.getString().startsWith("\nDISCORD SERVER") || text.getString().startsWith("\nSERVER STORE") || text.getString().startsWith("\nSUGGEST IDEAS"),true));
+        FILTERS.put("crate_open", new ChatFilter("crate_open", (Text text) -> text.getString().contains("\nPurchase keys:"), true));
+
+        FILTERS.put("lottery", new ChatFilter("lottery", (Text text) -> text.getString().startsWith("LOTTERY"), true));
+        FILTERS.put("coinflip", new ChatFilter("coinflip", (Text text) -> text.getString().contains("ᴄᴏɪɴꜰʟɪᴘ"), true));
+
+        FILTERS.put("mofood_charity", new ChatFilter("mofood_charity", (Text text) -> text.getString().startsWith("Charity"), true));
+        FILTERS.put("mofood_reroll_quest", new ChatFilter("mofood_reroll_quest", (Text text) -> text.getString().startsWith("MoFood") && text.getString().contains("rerolled the Quest"), true));
+        FILTERS.put("mofood_new_season", new ChatFilter("mofood_new_season", (Text text) -> text.getString().startsWith("▅") && text.getString().contains("New Season"), true));
+
         FILTERS.put("other", new ChatFilter("other", (Text text) -> {
             for(ChatFilter filter : FILTERS.values()) {
                 if(Objects.equals(filter.getName(), "other")) continue;
@@ -36,6 +53,23 @@ public class ChatFilter {
             }
             return true;
         }, true));
+    }
+
+    static {
+        CONFIGURABLE_FILTERS.put("player_join", () -> CavernChatConfig.getInstance().joinLeaveMessages.playerJoinMessage);
+        CONFIGURABLE_FILTERS.put("player_leave", () -> CavernChatConfig.getInstance().joinLeaveMessages.playerLeaveMessage);
+        CONFIGURABLE_FILTERS.put("new_player", () -> CavernChatConfig.getInstance().joinLeaveMessages.newPlayerMessage);
+
+        CONFIGURABLE_FILTERS.put("vote", () -> CavernChatConfig.getInstance().serverMessages.vote);
+        CONFIGURABLE_FILTERS.put("motd", () -> CavernChatConfig.getInstance().serverMessages.motd);
+        CONFIGURABLE_FILTERS.put("crate_open", () -> CavernChatConfig.getInstance().serverMessages.crateOpen);
+
+        CONFIGURABLE_FILTERS.put("lottery", () -> CavernChatConfig.getInstance().pluginMessages.lottery);
+        CONFIGURABLE_FILTERS.put("coinflip", () -> CavernChatConfig.getInstance().pluginMessages.coinFlip);
+
+        CONFIGURABLE_FILTERS.put("mofood_charity", () -> CavernChatConfig.getInstance().moFoodMessages.moFoodCharity);
+        CONFIGURABLE_FILTERS.put("mofood_reroll_quest", () -> CavernChatConfig.getInstance().moFoodMessages.moFoodRerollQuest);
+        CONFIGURABLE_FILTERS.put("mofood_new_season", () -> CavernChatConfig.getInstance().moFoodMessages.moFoodNewSeason);
     }
 
     private final String name;
@@ -48,10 +82,16 @@ public class ChatFilter {
 
     public void enable() {
         status = true;
+        if(Objects.equals(name, "other")) {
+            for(String filterKey : DYNAMIC_FILTERS) FILTERS.get(filterKey).enable();
+        }
         ChatVisibilityController.chatRebuild();
     }
     public void disable() {
         status = false;
+        if(Objects.equals(name, "other")) {
+            for(String filterKey : DYNAMIC_FILTERS) FILTERS.get(filterKey).disable();
+        }
         ChatVisibilityController.chatRebuild();
     }
 
