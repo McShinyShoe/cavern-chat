e9b12f78bfc4654ab218eeacb812b872b6c7027c
McShinyShoe <mcshinyshoe@protonmail.com>
2026-01-26 13:37:05 +0700

    feat: Configured the language

--- src/client/java/net/shinyshoe/cavernChat/client/CavernChatConfig.java
+++ src/client/java/net/shinyshoe/cavernChat/client/CavernChatConfig.java
@@ -22,11 +22,11 @@ public class CavernChatConfig implements ConfigData {
     @ConfigEntry.Gui.CollapsibleObject
     public MoFoodMessages moFoodMessages = new MoFoodMessages();
     @ConfigEntry.Gui.CollapsibleObject
-    public ColorfulDirectMessages colorfulDirectMessages = new ColorfulDirectMessages();
-    @ConfigEntry.Gui.CollapsibleObject
     public SlimefunMessages slimefunMessages = new SlimefunMessages();
     @ConfigEntry.Gui.CollapsibleObject
     public DungeonMessages dungeonMessages = new DungeonMessages();
+    @ConfigEntry.Gui.CollapsibleObject
+    public ColorfulDirectMessages colorfulDirectMessages = new ColorfulDirectMessages();
 
 
     public static class ChatChannels {
@@ -73,11 +73,6 @@ public class CavernChatConfig implements ConfigData {
         public FilterMode moFoodNewSeason = FilterMode.DYNAMIC;
     }
 
-    public static class ColorfulDirectMessages {
-        public boolean colorfulDirectMessageEnabled = true;
-        public List<DirectMessageColor> colorfulDirectMessageColors= new ArrayList<>();
-    }
-
     public static class SlimefunMessages {
         @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
         public FilterMode slimefunItemDisabled = FilterMode.DYNAMIC;
@@ -86,6 +81,11 @@ public class CavernChatConfig implements ConfigData {
     public static class DungeonMessages {
         @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
         public FilterMode dungeonsDrownedPirate = FilterMode.DYNAMIC;
+    }
+
+    public static class ColorfulDirectMessages {
+        public boolean colorfulDirectMessageEnabled = true;
+        public List<DirectMessageColor> colorfulDirectMessageColors= new ArrayList<>();
     }
 
     public static CavernChatConfig getInstance() {
